b2b76634796ba77397c92423b1f438fdc2f848cb
Antal Spector-Zabusky <antal.b.sz@gmail.com>
2022-09-13 03:45:37 +0200

    Bootstrap

--- typing/typedtree.mli
+++ typing/typedtree.mli
@@ -267,7 +267,7 @@ and expression_desc =
   | Texp_setfield of
       expression * Longident.t loc * Types.label_description * expression
   | Texp_array of expression list
-  | Texp_immutable_array of expression list
+  | Texp_immutable_array of expression list (* CR aspectorzabusky: Or [Texp_iarray]? *)
   | Texp_list_comprehension of comprehension
   | Texp_array_comprehension of comprehension
   | Texp_ifthenelse of expression * expression * expression option
