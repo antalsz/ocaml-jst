e1d5889299c8fc693afffbbfb9f5543a96b30f04
Antal Spector-Zabusky <antal.b.sz@gmail.com>
2022-09-19 17:00:38 +0100

    Some tests had variable/type numbers change – I *think* this is OK?

--- testsuite/tests/shapes/rotor_example.ml
+++ testsuite/tests/shapes/rotor_example.ml
@@ -26,7 +26,7 @@ end
 {
  "Pair"[module] ->
      Abs<.9>
-        (X/279, Abs(Y/280, {
+        (X/281, Abs(Y/282, {
                             "t"[type] -> <.5>;
                             "to_string"[value] -> <.6>;
                             }));
